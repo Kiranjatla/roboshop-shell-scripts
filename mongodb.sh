2f91802e2031170e2bd127262217f6f345db2570
KIRAN <ks@gmail.com>
2022-09-15 21:56:21 +0530

    Setting up ip address

--- mongodb.sh
+++ mongodb.sh
@@ -8,12 +8,17 @@ yum install -y mongodb-org &>>$LOG_FILE
 echo status=$?
 
 echo "Update mongodb listen ip address"
-sed -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 echo status=$?
 
 echo "Starting Mongodb server"
 systemctl enable mongod &>>$LOG_FILE
 systemctl start mongod &>>$LOG_FILE
 echo status=$?
+
+echo "Update mongodb listen ip address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+echo status=$?
+
 
 
