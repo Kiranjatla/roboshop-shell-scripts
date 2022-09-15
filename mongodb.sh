3882e1c19d74a801de7a4ae5fba98a94a02e79ef
KIRAN <ks@gmail.com>
2022-09-15 21:42:46 +0530

    echo special variables

--- mongodb.sh
+++ mongodb.sh
@@ -7,6 +7,10 @@ echo "Installing Mongodb server"
 yum install -y mongodb-org &>>$LOG_FILE
 echo status=$?
 
+echo "Update mongodb listen ip address"
+sed -i -e  's/127.0.0.0/0.0.0.0/' /etc/mongod.conf
+echo status=$?
+
 echo "Starting Mongodb server"
 systemctl enable mongod &>>$LOG_FILE
 systemctl start mongod &>>$LOG_FILE
