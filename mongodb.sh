79dd60e960c4279a9cc747c540739c32cfaa451c
KIRAN <ks@gmail.com>
2022-09-15 21:51:22 +0530

    Setting up ip address

--- mongodb.sh
+++ mongodb.sh
@@ -1,4 +1,4 @@
- LOG_FILE=/tmp/mongodb
+LOG_FILE=/tmp/mongodb
 echo "Setting Mongodb Repos File"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG_FILE
 echo status=$?
@@ -8,7 +8,7 @@ yum install -y mongodb-org &>>$LOG_FILE
 echo status=$?
 
 echo "Update mongodb listen ip address"
-sed -i -e  's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 echo status=$?
 
 echo "Starting Mongodb server"
