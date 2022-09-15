fb963bff75ab7e2a298a4015bda8eb228f48661a
KIRAN <ks@gmail.com>
2022-09-15 22:17:23 +0530

    Setting up ip address

--- mongodb.sh
+++ mongodb.sh
@@ -9,16 +9,34 @@ echo status=$?
 
 echo "Update mongodb listen ip address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-echo status=$?
+echo status = $?
 
 echo "Starting Mongodb server"
 systemctl enable mongod &>>$LOG_FILE
 systemctl start mongod &>>$LOG_FILE
 echo status=$?
 
-echo "Update mongodb listen ip address"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-echo status=$?
+echo "Downloading Mongodb Schema"
+ curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG_FILE
+ echo status-$?
+
+  cd /tmp
+  echo "Extracting Schema files"
+  unzip mongodb.zip &>>$LOG_FILE
+  echo status-$?
+  cd mongodb-main
+
+  echo"Load Catalogue service schema"
+  mongo < catalogue.js &>>$LOG_FILE
+  echo status-$?
+
+  echo "Load users  service schema"
+  mongo < users.js &>>$LOG_FILE
+  echo status-$?
+
+
+
+
 
 
 
