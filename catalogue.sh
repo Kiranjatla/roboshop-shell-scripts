938f40bf75d8546bd6e0672624fa4cbf44736a3f
KIRAN <ks@gmail.com>
2022-09-18 12:39:56 +0530

    colours

--- catalogue.sh
+++ catalogue.sh
@@ -7,55 +7,60 @@ fi
 
  echo "Setup Nodejs repos"
  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG_FILE}
- echo status=$?
+ if [ $? -eq 0 ] ; then
+     echo -e status = "\e[32mSuccess\e[0m"
+   else
+     echo -e status = "\e[31mSuccess\e[0m"
+     exit 1
+    fi
 
  echo "Installing Nodejs"
  yum install nodejs -y &>>${LOG_FILE}
- if [ $? -eq 0 ] ; then
-    echo status = success
-  else
-    echo status = failure
-    exit 1
-   fi
+  if [ $? -eq 0 ] ; then
+      echo -e status = "\e[32mSuccess\e[0m"
+    else
+      echo -e status = "\e[31mSuccess\e[0m"
+      exit 1
+     fi
 
 id roboshop &>>${LOG_FILE}
 if [ $? -ne 0 ]; then
  echo "Add Roboshop application user"
  user add roboshop &>>${LOG_FILE}
- if [ $? -eq 0 ] ; then
-     echo status = success
-   else
-     echo status = failure
-     exit 1
-    fi
+  if [ $? -eq 0 ] ; then
+      echo -e status = "\e[32mSuccess\e[0m"
+    else
+      echo -e status = "\e[31mSuccess\e[0m"
+      exit 1
+     fi
 fi
 echo "Download catalogue application code"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${LOG_FILE}
-if [ $? -eq 0 ] ; then
-    echo status = success
-  else
-    echo status = failure
-    exit 1
-   fi
+ if [ $? -eq 0 ] ; then
+     echo -e status = "\e[32mSuccess\e[0m"
+   else
+     echo -e status = "\e[31mSuccess\e[0m"
+     exit 1
+    fi
  cd /home/roboshop
 
  echo "Clean old catlaogue app content"
   rm -rf catalogue &>>${LOG_FILE}
-  if [ $? -eq 0 ] ; then
-       echo status = success
+   if [ $? -eq 0 ] ; then
+       echo -e status = "\e[32mSuccess\e[0m"
      else
-       echo status = failure
+       echo -e status = "\e[31mSuccess\e[0m"
        exit 1
       fi
 
  echo "Extracting catalogue application code"
  unzip /tmp/catalogue.zip &>>${LOG_FILE}
- if [ $? -eq 0 ] ; then
-     echo status = success
-   else
-     echo status = failure
-     exit 1
-    fi
+  if [ $? -eq 0 ] ; then
+      echo -e status = "\e[32mSuccess\e[0m"
+    else
+      echo -e status = "\e[31mSuccess\e[0m"
+      exit 1
+     fi
 
  mv catalogue-main catalogue
  cd /home/roboshop/catalogue
@@ -63,28 +68,28 @@ if [ $? -eq 0 ] ; then
  echo "Install NodeJS Dependencies"
  npm install &>>${LOG_FILE}
  if [ $? -eq 0 ] ; then
-     echo status = success
+     echo -e status = "\e[32mSuccess\e[0m"
    else
-     echo status = failure
+     echo -e status = "\e[31mSuccess\e[0m"
      exit 1
     fi
 
 echo "Setup Catalogue service"
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>${LOG_FILE}
-if [ $? -eq 0 ] ; then
-    echo status = success
-  else
-    echo status = failure
-    exit 1
-   fi
+ if [ $? -eq 0 ] ; then
+     echo -e status = "\e[32mSuccess\e[0m"
+   else
+     echo -e status = "\e[31mSuccess\e[0m"
+     exit 1
+    fi
  systemctl daemon-reload &>>${LOG_FILE}
  systemctl enable catalogue &>>${LOG_FILE}
 
  echo "Start catalogue service"
  systemctl start catalogue &>>${LOG_FILE}
-if [ $? -eq 0 ] ; then
-    echo status = success
-  else
-    echo status = failure
-    exit 1
-   fi
+ if [ $? -eq 0 ] ; then
+     echo -e status = "\e[32mSuccess\e[0m"
+   else
+     echo -e status = "\e[31mSuccess\e[0m"
+     exit 1
+    fi
