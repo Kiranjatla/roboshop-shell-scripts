55cacd446a6661e8ba4feaf9e19144f36bfbf9ee
KIRAN <ks@gmail.com>
2022-09-18 21:13:41 +0530

    user commit final

--- common.sh
+++ common.sh
@@ -52,7 +52,7 @@ NODEJS(){
 }
 
 echo "Update SystemD service file"
-    sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/roboshop/user/systemd.service
+    sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service
     statuscheck $?
 
     echo "Setup ${COMPONENT} service"
