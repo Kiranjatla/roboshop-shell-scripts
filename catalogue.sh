e2fc9c57e6d416c3f2657370fd7c8862569e5ba6
KIRAN <ks@gmail.com>
2022-09-18 11:03:46 +0530

    if else Conditions

--- catalogue.sh
+++ catalogue.sh
@@ -5,36 +5,64 @@ LOG_FILE=/tmp/catalogue
 
  echo "Installing Nodejs"
  yum install nodejs -y &>>${LOG_FILE}
- echo status=$?
+ if [$? -eq 0]; then
+   echo status=success
+   else
+   echo status=failure
+   fi
 
  echo "Add Roboshop application user"
  user add roboshop &>>${LOG_FILE}
- echo status=$?
+ if [$? -eq 0]; then
+    echo status=success
+    else
+    echo status=failure
+    fi
 
 echo "Download catalogue application code"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${LOG_FILE}
-echo status=$?
+if [$? -eq 0]; then
+   echo status=success
+   else
+   echo status=failure
+   fi
 
  cd /home/roboshop
 
  echo "Extracting catalogue application code"
  unzip /tmp/catalogue.zip &>>${LOG_FILE}
- echo status=$?
+ if [$? -eq 0]; then
+    echo status=success
+    else
+    echo status=failure
+    fi
 
  mv catalogue-main catalogue
  cd /home/roboshop/catalogue
 
  echo "Install NodeJS Dependencies"
  npm install &>>${LOG_FILE}
- echo status=$?
+ if [$? -eq 0]; then
+    echo status=success
+    else
+    echo status=failure
+    fi
 
 echo "Setup Catalogue service"
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>${LOG_FILE}
-echo status=$?
+if [$? -eq 0]; then
+   echo status=success
+   else
+   echo status=failure
+   fi
 
  systemctl daemon-reload &>>${LOG_FILE}
  systemctl enable catalogue &>>${LOG_FILE}
 
  echo "Start catalogue service"
  systemctl start catalogue &>>${LOG_FILE}
-echo status=$?
+if [$? -eq 0]; then
+   echo status=success
+   else
+   echo status=failure
+   fi
