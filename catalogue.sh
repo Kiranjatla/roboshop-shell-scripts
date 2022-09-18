e0ad8bee917d826cb9020c9cac0cd41441c140df
KIRAN <ks@gmail.com>
2022-09-18 10:28:55 +0530

    Catalogue script issues resolved

--- catalogue.sh
+++ catalogue.sh
@@ -3,7 +3,7 @@ LOG_FILE=/tmp/catalogue
  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG_FILE}
  echo status=$?
 
- echo"Installing Nodejs"
+ echo "Installing Nodejs"
  yum install nodejs -y &>>${LOG_FILE}
  echo status=$?
 
@@ -28,13 +28,13 @@ echo status=$?
  npm install &>>${LOG_FILE}
  echo status=$?
 
-echo"Setup Catalogue service"
+echo "Setup Catalogue service"
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>${LOG_FILE}
 echo status=$?
 
  systemctl daemon-reload &>>${LOG_FILE}
  systemctl enable catalogue &>>${LOG_FILE}
 
- echo"Start catalogue service"
+ echo "Start catalogue service"
  systemctl start catalogue &>>${LOG_FILE}
 echo status=$?
