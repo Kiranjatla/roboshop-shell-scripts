3a6ecfe23131cddffb5c668e977874209a3eef71
KIRAN <ks@gmail.com>
2022-09-18 19:47:56 +0530

    redis commit

--- frontend.sh
+++ frontend.sh
@@ -1,30 +1,30 @@
- LOG_FILES=/tmp/frontend
+ LOG_FILE=/tmp/frontend
  source common.sh
 
  echo Installing Nginx software
- yum install nginx -y &>>$LOG_FILES
+ yum install nginx -y &>>$LOG_FILE
  statuscheck $?
 
  echo Downloading Nginx Web Content
- curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILES
+ curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
  statuscheck $?
  cd /usr/share/nginx/html
 
  echo Removing Old Web Content
- rm -rf * &>>$LOG_FILES
+ rm -rf * &>>$LOG_FILE
  statuscheck $?
 
  echo Extracting Web Content
- unzip /tmp/frontend.zip &>>$LOG_FILES
+ unzip /tmp/frontend.zip &>>$LOG_FILE
  statuscheck $?
 
- mv frontend-main/static/* . &>>$LOG_FILES
- mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILES
+ mv frontend-main/static/* . &>>$LOG_FILE
+ mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
 
  echo Starting Nginx Service
- systemctl enable nginx &>>$LOG_FILES
+ systemctl enable nginx &>>$LOG_FILE
  statuscheck $?
- systemctl start nginx &>>$LOG_FILES
+ systemctl start nginx &>>$LOG_FILE
  statuscheck $?
- systemctl restart nginx &>>$LOG_FILES
+ systemctl restart nginx &>>$LOG_FILE
  statuscheck $?
