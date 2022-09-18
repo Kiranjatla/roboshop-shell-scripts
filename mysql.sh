9e49fd3afd2fbc88bf43f982d9ff1beb479ec301
KIRAN <ks@gmail.com>
2022-09-18 23:01:26 +0530

    My SQL script

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,6 @@
 LOG_FILE=/tmp/mysql
-
  source common.sh
+
  echo "setting up  MySQL Repo file"
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>$LOG_FILE
 statuscheck $?
@@ -16,14 +16,14 @@ statuscheck $?
  systemctl start mysqld &>>$LOG_FILE
  statuscheck $?
 
- DEFAULT_PASSWORD=$(sudo grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
+ DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
  echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${ROBOSHOP_MYSQL_PASSWORD}');
  FLUSH PRIVILEGES;" >/tmp/root-pass.sql
 
-#echo "Change the default root password"
-#mysql -uroot -p "${DEFAULT_PASSWORD}" </tmp/root-pass.sql &>>$LOG_FILE
-#statuscheck $?
+ echo "Change the default root password"
+ mysql -uroot -p "${DEFAULT_PASSWORD}" </tmp/root-pass.sql &>>$LOG_FILE
+ statuscheck $?
 
  #mysql_secure_installation
 # mysql -uroot -pRoboShop@1
