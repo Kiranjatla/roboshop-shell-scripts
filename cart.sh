ade77a3646dfce3178484144e8e1be60b12019dd
KIRAN <ks@gmail.com>
2022-09-18 21:20:05 +0530

    cart script

--- cart.sh
+++ cart.sh
@@ -1,22 +1,5 @@
- LOG_FILE=/tmp/cart
+ COMPONENT=cart
+ LOG_FILE=/tmp/${COMPONENT}
 
-  source common.sh
-
-  echo"Adding Rpos File"
-  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>LOG_FILE
-  statuscheck $?
-
-  echo"Installing Nodejs"
-  yum install nodejs -y &>>LOG_FILE
-  statuscheck $?
-
-  echo"Add Roboshop User"
-  useradd roboshop &>>LOG_FILE
-  statuscheck $?
-
-   curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
-   cd /home/roboshop
-   unzip /tmp/cart.zip
-   mv cart-main cart
-   cd cart
-   npm install
+    source common.sh
+    NODEJS
