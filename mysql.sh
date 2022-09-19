43e8406e023999da30626b44ef5366f5d9fffdae
KIRAN <ks@gmail.com>
2022-09-19 22:18:29 +0530

    My SQL script modified

--- mysql.sh
+++ mysql.sh
@@ -16,17 +16,17 @@ statuscheck $?
  systemctl start mysqld &>>$LOG_FILE
  statuscheck $?
 
- DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
+ DEFAULT_PASSWORD=$( sudo grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
- #echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${ROBOSHOP_MYSQL_PASSWORD}');
- #FLUSH PRIVILEGES;" >/tmp/root-pass-sql
+ echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${ROBOSHOP_MYSQL_PASSWORD}');
+ FLUSH PRIVILEGES;" >/tmp/root-pass-sql
 
- #echo "Show databases;" |mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} &>>$LOG_FILE
- #if [ $? -ne 0 ]; then
- #echo "Change the default root password"
- #mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}"  </tmp/root-pass.sql &>>$LOG_FILE
- #statuscheck $?
- #fi
+ echo "Show databases;" |mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} &>>$LOG_FILE
+ if [ $? -ne 0 ]; then
+ echo "Change the default root password"
+ mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}"  </tmp/root-pass.sql &>>$LOG_FILE
+ statuscheck $?
+ fi
 
 echo 'show plugins'|mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} 2>/dev/null | grep validate_password &>>$LOG_FILE
 if [ $? -eq 0 ]; then
