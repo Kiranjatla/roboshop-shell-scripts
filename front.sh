89250cca4bf3486539f5f8d3db2680be1b67f12c
KIRAN <ks@gmail.com>
2025-03-15 10:46:07 +0530

    Adding redirectors

--- front.sh
+++ front.sh
@@ -1,21 +1,21 @@
  echo Installing Nginx software
- yum install nginx -y &>>$/tmp/frontend
+ yum install nginx -y &>>/tmp/frontend
 
 
  echo Downloading Nginx Web Content
- curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$/tmp/frontend
+ curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>/tmp/frontend
 
  cd /usr/share/nginx/html
 
  echo Removing Old Web Content
- rm -rf * &>>$/tmp/frontend
+ rm -rf * &>>/tmp/frontend
 
  echo Extracting Web Content
- unzip /tmp/frontend.zip &>>$/tmp/frontend
+ unzip /tmp/frontend.zip &>>/tmp/frontend
 
- mv frontend-main/static/* . &>>$/tmp/frontend
- mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$/tmp/frontend
+ mv frontend-main/static/* . &>>/tmp/frontend
+ mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/frontend
 
  echo Starting Nginx Service
- systemctl enable nginx &>>$/tmp/frontend
- systemctl restart nginx &>>$/tmp/frontend
+ systemctl enable nginx &>>/tmp/frontend
+ systemctl restart nginx &>>/tmp/frontend
