7d4917a6fd99efbc8b6e393ac73bd07aad211be6
KIRAN <ks@gmail.com>
2025-03-16 12:22:57 +0530

    Mongodb update IP Address using SED Substitution

--- mongodb.sh
+++ mongodb.sh
@@ -7,6 +7,10 @@
  yum install -y mongodb-org &>>$LOG_FILE
  echo status = $?
 
+ echo "update MongoDB Listen Ip Address"
+ sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+ echo status = $?
+
  echo "Enabled mongodb server"
  systemctl enable mongod &>>$LOG_FILE
  echo status = $?
