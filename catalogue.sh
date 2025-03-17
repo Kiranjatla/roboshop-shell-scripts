aadb2089bf28781467bf3395bb50ba0eb85706ee
KIRAN <ks@gmail.com>
2025-03-17 21:36:39 +0530

    sudo privileges script

--- catalogue.sh
+++ catalogue.sh
@@ -1,38 +1,45 @@
 LOG_FILE=/tmp/catalogue
+
+ID=$(id -u)
+if [ $ID -ne 0 ]; then
+  echo you should run this as root user (or) with sudo previleges
+  exit 1
+   fi
+
 echo "setup nodejs repos"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>LOG_FILE
 if [ $? -eq 0 ]; then
   echo status = success
-  else
-  echo status - failure
-  exit
+else
+  echo status = failure
+  exit 1
   fi
 
 echo "Install nodejs"
- yum install nodejs -y &>>LOG_FILE
+yum install nodejs -y &>>LOG_FILE
 if [ $? -eq 0 ]; then
   echo status = success
-  else
-  echo status - failure
-  exit
+else
+  echo status = failure
+  exit 1
   fi
 
 echo "Adding Roboshop application User"
 useradd roboshop &>>LOG_FILE
 if [ $? -eq 0 ]; then
   echo status = success
-  else
-  echo status - failure
-  exit
+else
+  echo status = failure
+  exit 1
   fi
 
 echo "Download catalogue application code"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>LOG_FILE
 if [ $? -eq 0 ]; then
   echo status = success
-  else
-  echo status - failure
-  exit
+else
+  echo status = failure
+  exit 1
   fi
 
 cd /home/roboshop
@@ -41,9 +48,9 @@ echo "extract catalogue application code"
 unzip /tmp/catalogue.zip &>>LOG_FILE
 if [ $? -eq 0 ]; then
   echo status = success
-  else
-  echo status - failure
-  exit
+else
+  echo status = failure
+  exit 1
   fi
 
 mv catalogue-main catalogue
@@ -53,18 +60,18 @@ echo "Install Nodejs Dependencies"
 npm install &>>LOG_FILE
 if [ $? -eq 0 ]; then
   echo status = success
-  else
-  echo status - failure
-  exit
+else
+  echo status = failure
+  exit 1
   fi
 
 echo "Setup Catalogue Service"
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>LOG_FILE
 if [ $? -eq 0 ]; then
   echo status = success
-  else
-  echo status - failure
-  exit
+else
+  echo status = failure
+  exit 1
   fi
 
 systemctl daemon-reload &>>LOG_FILE
@@ -74,7 +81,7 @@ echo "Restart the Servcie"
 systemctl start catalogue &>>LOG_FILE
 if [ $? -eq 0 ]; then
   echo status = success
-  else
-  echo status - failure
-  exit
+else
+  echo status = failure
+  exit 1
   fi
