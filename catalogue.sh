ba02233f4b843d247d4de7a493eb53c04b984c63
KIRAN <ks@gmail.com>
2025-03-17 21:41:55 +0530

    sudo privileges script

--- catalogue.sh
+++ catalogue.sh
@@ -7,7 +7,7 @@ if [ $ID -ne 0 ]; then
    fi
 
 echo "setup nodejs repos"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>LOG_FILE
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG_FILE}
 if [ $? -eq 0 ]; then
   echo status = success
 else
@@ -16,7 +16,7 @@ else
   fi
 
 echo "Install nodejs"
-yum install nodejs -y &>>LOG_FILE
+yum install nodejs -y &>>${LOG_FILE}
 if [ $? -eq 0 ]; then
   echo status = success
 else
@@ -25,7 +25,7 @@ else
   fi
 
 echo "Adding Roboshop application User"
-useradd roboshop &>>LOG_FILE
+useradd roboshop &>>${LOG_FILE}
 if [ $? -eq 0 ]; then
   echo status = success
 else
@@ -34,7 +34,7 @@ else
   fi
 
 echo "Download catalogue application code"
-curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>LOG_FILE
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${LOG_FILE}
 if [ $? -eq 0 ]; then
   echo status = success
 else
@@ -45,7 +45,7 @@ else
 cd /home/roboshop
 
 echo "extract catalogue application code"
-unzip /tmp/catalogue.zip &>>LOG_FILE
+unzip /tmp/catalogue.zip &>>${LOG_FILE}
 if [ $? -eq 0 ]; then
   echo status = success
 else
@@ -57,7 +57,7 @@ mv catalogue-main catalogue
 cd /home/roboshop/catalogue
 
 echo "Install Nodejs Dependencies"
-npm install &>>LOG_FILE
+npm install &>>${LOG_FILE}
 if [ $? -eq 0 ]; then
   echo status = success
 else
@@ -66,7 +66,7 @@ else
   fi
 
 echo "Setup Catalogue Service"
-mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>LOG_FILE
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>${LOG_FILE}
 if [ $? -eq 0 ]; then
   echo status = success
 else
@@ -74,11 +74,11 @@ else
   exit 1
   fi
 
-systemctl daemon-reload &>>LOG_FILE
-systemctl enable catalogue &>>LOG_FILE
+systemctl daemon-reload &>>${LOG_FILE}
+systemctl enable catalogue &>>${LOG_FILE}
 
 echo "Restart the Servcie"
-systemctl start catalogue &>>LOG_FILE
+systemctl start catalogue &>>${LOG_FILE}
 if [ $? -eq 0 ]; then
   echo status = success
 else
