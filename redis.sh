cbac898c6e2f40dbf268a06eb1e95c4942ba622c
KIRAN <ks@gmail.com>
2025-03-18 19:20:19 +0530

    redis

--- redis.sh
+++ redis.sh
@@ -2,7 +2,7 @@ LOG_FILE=/tmp/redis
 
 source common.sh
 
-echo "Setup YUM Repos for Redis"
+#echo "Setup YUM Repos for Redis"
 # THis remi repos is not working any more, So ignroe this step and move to next step directly. Run the commands from next step.
 #dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>$LOG_FILE
 #StatusCheck $?
@@ -11,13 +11,14 @@ echo "disabling Redis YUM Modules"
 dnf module disable redis -y &>>$LOG_FILE
 StatusCheck $?
 
+echo "install redis"
+dnf install redis -y  &>>$LOG_FILE
+StatusCheck $?
+
 echo "enable Redis module"
 dnf module enable redis:6 -y &>>$LOG_FILE
 StatusCheck $?
 
-echo "install redis"
-dnf install redis -y  &>>$LOG_FILE
-StatusCheck $?
 
 echo "Update Redis Listen address from 127.0.0.1 to 0.0.0.0"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
