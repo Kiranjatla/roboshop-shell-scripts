d75431d9f4d86dfdc9aac43c9450e6edd21abf97
KIRAN <ks@gmail.com>
2025-03-18 19:01:59 +0530

    redis

--- redis.sh
+++ redis.sh
@@ -3,27 +3,30 @@ LOG_FILE=/tmp/redis
 source common.sh
 
 echo "Setup YUM Repos for Redis"
-dnf install https://rpms.remirepo.net/enterprise/remi-release-8.9.rpm/etc/ -y &>>$LOG_FILE
+# THis remi repos is not working any more, So ignroe this step and move to next step directly. Run the commands from next step.
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>$LOG_FILE
 StatusCheck $?
 
-echo "Enabling Redis YUM Modules"
-dnf install remi-release-8.9.rpm -y &>>$LOG_FILE
+echo "disabling Redis YUM Modules"
+dnf module disable redis -y &>>$LOG_FILE
 StatusCheck $?
 
-echo "Install Redis"
-yum install redis -y &>>$LOG_FILE
+echo "enable Redis module"
+dnf module enable redis:6 -y &>>$LOG_FILE
 StatusCheck $?
 
-echo "start redis"
-systemctl start redis &>>$LOG_FILE
+echo "install redis"
+dnf install redis -y  &>>$LOG_FILE
 StatusCheck $?
 
 echo "Update Redis Listen address from 127.0.0.1 to 0.0.0.0"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
 StatusCheck $?
 
+echo "enable redis"
 systemctl enable redis &>>$LOG_FILE
+StatusCheck $?
 
 echo "Start Redis"
-systemctl restart redis &>>$LOG_FILE
+systemctl start redis &>>$LOG_FILE
 StatusCheck $?
