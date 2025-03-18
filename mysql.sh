dcd056b7d83079df0ad1128fbb6e42b692e44cce
KIRAN <ks@gmail.com>
2025-03-18 22:38:56 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -1,33 +1,33 @@
  LOG_FILE=/tmp/mysql
  source common.sh
 
- echo "settingup mysql repo file"
- curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>${LOG_FILE}
+ echo "Setting Up MySQL Repo"
+ curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>$LOG_FILE
  StatusCheck $?
 
- echo "disable mysql default module to enable 5.7 mysql"
- dnf module disable mysql -y &>>${LOG_FILE}
+ echo "Disable MySQL Default Module to Enable 5.7 MySQL"
+ dnf module disable mysql -y &>>$LOG_FILE
  StatusCheck $?
 
- echo "install mysql"
- yum install mysql-community-server -y &>>${LOG_FILE}
+ echo "Install MySQL"
+ yum install mysql-community-server -y &>>$LOG_FILE
  StatusCheck $?
 
- echo "Start mysql service"
- systemctl enable mysqld &>>${LOG_FILE}
- systemctl restart mysqld &>>${LOG_FILE}
+ echo "Start MySQL Service"
+ systemctl enable mysqld &>>$LOG_FILE
+ systemctl restart mysqld &>>$LOG_FILE
  StatusCheck $?
 
  DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
- echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD ('${ROBOSHOP_MYSQL_PASSWORD}');
+ echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${ROBOSHOP_MYSQL_PASSWORD}');
  FLUSH PRIVILEGES;" >/tmp/root-pass.sql
 
- echo "show databases;" |mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} &>>${LOG_FILE}
+ echo "show databases;" |mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD} &>>$LOG_FILE
  if [ $? -ne 0 ]; then
- echo "Change the default root password"
- mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}" </tmp/root-pass.sql &>>${LOG_FILE}
- StatusCheck $?
+   echo "Change the default root password"
+   mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}" </tmp/root-pass.sql &>>$LOG_FILE
+   StatusCheck $?
  fi
 
 
