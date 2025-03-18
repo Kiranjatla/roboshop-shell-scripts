42c485201ebe6798e4d6fa1cdd2d7564b4730a29
KIRAN <ks@gmail.com>
2025-03-18 22:08:02 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -1,12 +1,12 @@
- LOG_FILE=/tmp/user
+ LOG_FILE=/tmp/mysql
  source common.sh
 
- eho "settingup mysql repo file"
+ echo "settingup mysql repo file"
  curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>${LOG_FILE}
  StatusCheck $?
 
  echo "disable mysql default module to enable 5.7 mysql"
- dnf module disable mysql &>>${LOG_FILE}
+ dnf module disable mysql -y &>>${LOG_FILE}
  StatusCheck $?
 
  echo "install mysql"
@@ -18,8 +18,16 @@
  systemctl start mysqld &>>${LOG_FILE}
  StatusCheck $?
 
- #grep temp /var/log/mysqld.log
- #mysql_secure_installation
+ DEFAULT_PASSWORD=$(sudo grep 'A temp' /var/log/mysqld.log | awk '{print $11}')
+
+ echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${ROBOSHOP_MYSQL_PASSWORD}');
+ FLUSH PRIVILEGES;" >/tmp/root-pass.sql
+
+ echo "Change the default root password"
+ mysql -uroot -p${DEFAULT_PASSWORD} </tmp/roor-pass.sql &>>${LOG_FILE}
+ StatusCheck $?
+
+
  #mysql -uroot -pRoboShop@1
 #> uninstall plugin validate_password;
  #curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
