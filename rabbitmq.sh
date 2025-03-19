9f6ac95fc4aa15661db7502d41962e2c22a113c7
KIRAN <ks@gmail.com>
2025-03-19 19:14:05 +0530

    Rabbitmq

---
+++ rabbitmq.sh
@@ -0,0 +1,37 @@
+COMPONENT=rabbitmq
+source common.sh
+LOG_FILE=/tmp/${COMPONENT}
+
+echo "Setup RabbitMQ Repos"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash &>>$LOG_FILE
+StatusCheck $?
+
+echo "Install Erland "
+yum install erlang -y &>>$LOG_FILE
+StatusCheck $?
+
+echo "Install Yum Repositories for RabbitMQ"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>$LOG_FILE
+StatusCheck $?
+
+echo "Install Erland "
+yum install rabbitmq-server -y &>>$LOG_FILE
+StatusCheck $?
+
+
+echo "Start RabbitMQ Server"
+systemctl enable rabbitmq-server &>>$LOG_FILE
+systemctl restart rabbitmq-server &>>$LOG_FILE
+StatusCheck $?
+
+echo "add Application user in RabbitMQ"
+rabbitmqctl add_user roboshop roboshop123 &>>$LOG_FILE
+StatusCheck $?
+
+echo "add Application user tags in RabbitMQ"
+rabbitmqctl set_user_tags roboshop administrator &>>$LOG_FILE
+StatusCheck $?
+
+echo "add Application user tags in RabbitMQ"
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG_FILE
+StatusCheck $?
